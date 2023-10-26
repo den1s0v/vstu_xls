cba24dab3caf75184b5ecf4856e20e688b5b9243
den1s0v <hello-lumen@ya.ru>
2023-10-27 00:23:43 +0300

    Separate 2D and 1D geometry classes

---
+++ src/geom1d.py
@@ -0,0 +1,241 @@
+# geom1d.py
+
+""" Одномерный примитив (отрезок) для работы на координатной прямой """
+
+
+# Имитирует интерфейс tuple
+class LinearSegment:
+    """Отрезок AB на прямой (1D) - две целых координаты (a <= b)"""
+
+    def __init__(self, a, b=None, length=None):
+        if b is None:
+            assert length is not None, 'LinearSegment(a, [b] [,length]) ' \
+                                       'cannot be created without b and length specified.'
+            b = a + length
+        assert a <= b, 'Segment [{},{}] has negative length!'.format(a, b)
+        self.a = a
+        self.b = b
+        # super().__init__()
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.a
+        if index == 1:
+            return self.b
+        raise IndexError(index)
+
+    def __iter__(self):
+        return iter((self.a, self.b))
+
+    def __len__(self):
+        return 2
+
+
+class GenericRelation:
+    description = 'соотносится с'
+
+    def implies(cls, other_cls: type):
+        return issubclass(cls, other_cls)
+
+    def format_description(self, a=None, b=None, vertical=False):
+        s = self.description
+        if vertical:
+            s = s.replace('слева', 'сверху') \
+                .replace('справа', 'снизу') \
+                .replace('левую', 'верхнюю') \
+                .replace('правую', 'нижнюю')
+        if a and b:
+            if '{}' not in s:
+                s = '{} %s {}' % s
+            return s.format(a, b)
+        return s
+
+
+# * * Внутренние * * #
+
+class Overlaps(GenericRelation):
+    description = 'пересекается с'
+
+
+class Includes(Overlaps):
+    description = 'включает'
+
+
+class IncludedIn(Overlaps):
+    description = 'входит в'
+
+
+class TouchesInternally(IncludedIn):
+    description = 'имеет общую стенку'
+
+
+class TouchesInternallyLeft(TouchesInternally):
+    description = 'имеет общую левую стенку с'
+
+
+class TouchesInternallyRight(TouchesInternally):
+    description = 'имеет общую правую стенку с'
+
+
+class IncludesGluedLeft(TouchesInternallyLeft, Includes, ):
+    description = 'включает примыкающее слева'
+
+
+class IncludesGluedRight(TouchesInternallyRight, Includes, ):
+    description = 'включает примыкающее справа'
+
+
+class IncludedInGluesLeft(TouchesInternallyLeft, IncludedIn, ):
+    description = 'включается, примыкая слева, в'
+
+
+class IncludedInGluesRight(TouchesInternallyRight, IncludedIn, ):
+    description = 'включается, примыкая справа, в'
+
+
+class CoinsidesWith(IncludesGluedLeft, IncludesGluedRight, IncludedInGluesLeft, IncludedInGluesRight):
+    description = 'совпадает с'
+
+
+### Внешние ###
+
+class NoOverlap(GenericRelation):
+    description = 'не пересекается с'
+
+
+class AtLeftFrom(NoOverlap):
+    description = 'слева от'
+
+
+class AtRightFrom(NoOverlap):
+    description = 'справа от'
+
+
+class Touches(NoOverlap):
+    description = 'примыкает к'
+
+
+class TouchesAtLeft(Touches, AtRightFrom):
+    description = 'примыкает слева к'
+
+
+class TouchesAtRight(Touches, AtLeftFrom):
+    description = 'примыкает справа к'
+
+
+class FarFrom(NoOverlap):
+    description = 'на удалении от'
+
+
+class FarFromAtLeft(FarFrom, AtLeftFrom):
+    description = 'на удалении слева от'
+
+
+class FarFromAtRight(FarFrom, AtRightFrom):
+    description = 'на удалении справа от'
+
+
+class LinearRelation:
+    """" Способ описать пространственное взаимоотношение
+    одного отрезка на прямой (AB) по отношению к другому (CD)
+        Равны:
+            a----b
+            c----d
+        Включает:
+            a------b
+              c-d
+        Включается:
+              a-b
+            c-----d
+        Слева на расстоянии:
+            a----b
+                    c----d
+        Справа на расстоянии:
+                    a----b
+            c----d
+        Слева, примыкая:
+            a----b
+                 c----d
+        Справа, примыкая:
+                 a----b
+            c----d
+        Пересекает слева:
+            a-----b
+               c----d
+        Пересекает справа:
+               a----b
+            c----d
+
+    kind: class
+    description: str
+    equals: bool = None
+    includes: bool = None
+    included: bool = None
+    overlaps: bool = None
+
+    outer_gap: int = None
+    outer_gap_left: int = None
+    outer_gap_right: int = None
+    indent_inner_left: int = None
+    indent_inner_right: int = None
+    """
+
+    def __init__(self, s1: LinearSegment, s2: LinearSegment):
+        self.s1 = s1
+        self.s2 = s2
+        a, b = s1
+        c, d = s2
+        # find distance between points: c-a, b-d (inside)
+        # Внутренние отступы (левый и правый): >= 0, когда AB включает CD
+        il = c - a
+        ir = b - d
+        # find distance between points: a-d, c-b (outside)
+        # Внешние зазоры: >= 0, когда AB и CD не пересекаются (== 0: примыкают)
+        gl = a - d
+        gr = c - b
+
+        self.inner_offset_left = il
+        self.inner_offset_right = ir
+        self.outer_gap_left = gl
+        self.outer_gap_right = gr
+        self.outer_gap = max(gl, gr)
+        self.kind = GenericRelation
+        self.description = None
+
+        if il >= 0 <= ir:
+            self.includes = True
+            self.kind = Includes
+
+            if il == 0 and ir == 0:
+                self.included = True
+                self.equals = True
+                self.kind = CoinsidesWith
+            if il == 0 and ir > 0:
+                self.kind = IncludesGluedLeft
+            elif il > 0 and ir == 0:
+                self.kind = IncludesGluedRight
+
+        elif il <= 0 >= ir:
+            self.included = True
+            self.kind = IncludedIn
+
+            if il == 0 and ir < 0:
+                self.kind = IncludedInGluesLeft
+            elif il < 0 and ir == 0:
+                self.kind = IncludedInGluesRight
+
+        elif gl > 0:
+            self.kind = FarFromAtRight
+        elif gl == 0:
+            self.kind = TouchesAtRight  # AB касается левого края CD, будучи при этом справа от него
+
+        elif gr > 0:
+            self.kind = FarFromAtLeft
+        elif gr == 0:
+            self.kind = TouchesAtLeft
+        else:
+            # fallback
+            self.kind = Overlaps
+
+        self.description = self.kind.description
+
---
+++ src/test_geom1d.py
@@ -0,0 +1,129 @@
+import unittest
+from geom1d import LinearSegment, LinearRelation
+
+
+def get_relation(segment1, segment2) -> LinearRelation:
+    # return LinearRelation(LinearSegment(segment1), LinearSegment(segment2))
+    s1 = LinearSegment(*segment1)
+    s2 = LinearSegment(*segment2)
+    return LinearRelation(s1, s2)
+
+
+def str_to_segment(s: str):
+    return (
+        s.find('a'),
+        s.find('b'),
+    )
+
+
+def relation_from_two_strings(s1, s2) -> LinearRelation:
+    return get_relation(
+        str_to_segment(s1),
+        str_to_segment(s2),
+    )
+
+
+class RelationTestCase(unittest.TestCase):
+
+    ### Внутренние конфигурации ###
+
+    def test_relation_eq(self):
+        # self.assertEqual(True, False)  # add assertion here
+
+        r = relation_from_two_strings(
+            'a-----b',
+            'a-----b',
+        )
+        self.assertEqual('совпадает с', r.description)
+
+        r = relation_from_two_strings(
+            'ab',
+            'ab',
+        )
+        self.assertEqual('совпадает с', r.description)
+
+    def test_relation_in(self):
+        r = relation_from_two_strings(
+            'a----------b',
+            '  a-----b   ',
+        )
+        self.assertEqual('включает', r.description)  # охватывает (?)
+
+        r = relation_from_two_strings(
+            '  a-----b   ',
+            'a----------b',
+        )
+        self.assertEqual('входит в', r.description)
+
+    def test_relation_inner_touch(self):
+        r = relation_from_two_strings(
+            '     a-----b',
+            'a----------b',
+        )
+        self.assertEqual('включается, примыкая справа, в', r.description)
+
+        r = relation_from_two_strings(
+            'a----------b',
+            '     a-----b',
+        )
+        self.assertEqual('включает примыкающее справа', r.description)
+
+        r = relation_from_two_strings(
+            'a-----b     ',
+            'a----------b',
+        )
+        self.assertEqual('включается, примыкая слева, в', r.description)
+
+        r = relation_from_two_strings(
+            'a----------b',
+            'a-----b     ',
+        )
+        self.assertEqual('включает примыкающее слева', r.description)
+
+        ### конфигурации перекрытия ###
+
+    def test_relation_overlap(self):
+        r = relation_from_two_strings(
+            'a----------b    ',
+            '         a-----b',
+        )
+        self.assertEqual('пересекается с', r.description)
+
+        r = relation_from_two_strings(
+            '         a-----b',
+            'a----------b    ',
+        )
+        self.assertEqual('пересекается с', r.description)
+
+        ### Наружные конфигурации ###
+
+    def test_relation_far1(self):
+        r = relation_from_two_strings(
+            'a-----b           ',
+            '           a-----b',
+        )
+        self.assertEqual('на удалении слева от', r.description)
+
+    def test_relation_far2(self):
+        r = relation_from_two_strings(
+            '           a-----b',
+            'a-----b           ',
+        )
+        self.assertEqual('на удалении справа от', r.description)
+
+    def test_relation_outer_touch(self):
+        r = relation_from_two_strings(
+            '      a-----b',
+            'a-----b      ',
+        )
+        self.assertEqual('примыкает справа к', r.description)
+
+        r = relation_from_two_strings(
+            'a-----b      ',
+            '      a-----b',
+        )
+        self.assertEqual('примыкает слева к', r.description)
+
+
+if __name__ == '__main__':
+    unittest.main()
