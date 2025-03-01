0e4e26d80456a69ca03376b0d785f6bafec28e5f
den1s0v <hello-lumen@ya.ru>
2025-03-02 01:40:49 +0300

    Describe NonTerminal as generalization/extension_point

--- src/grammar2d/NonTerminal.py
+++ src/grammar2d/NonTerminal.py
@@ -8,18 +8,36 @@ from grammar2d.PatternComponent import PatternComponent
 # @dataclass(kw_only=True)
 @PatternRegistry.register
 class NonTerminal(Pattern2d):
-    """Структура или Коллекция"""
+    """Структура или Коллекция.
+
+    Этот тип паттерна не может быть обнаружен в данных непосредственно,
+    но может быть использован в грамматике с `kind=general`,
+    чтобы служить обобщением для других паттернов, которые указывают этот в своём списке `extends`.
+    Таким образом реализуется принцип полиморфизма и аналог множественного наследования.
+    """
 
     @classmethod
     @override
     def get_kind(cls):
+        """ ...Thoughts to give it a more sensible name:
+          - generalization;
+          - base;
+          - abstract;
+          - extension_point;
+          - ...?
+         """
         return "general"  # ???
 
     def dependencies(self, recursive=False) -> list[Pattern2d]:
         return []
 
+    # def max_score(self) -> float:
+    #     """ precision = score / max_score """
+    #     raise NotImplementedError(type(self))
+
     ...
 
-    def get_matcher(self, grammar_macher):
-        raise NotImplementedError(type(self))
-
+    def get_matcher(self, grammar_matcher):
+        # raise NotImplementedError(type(self))
+        from grammar2d.NonTerminalMatcher import NonTerminalMatcher
+        return NonTerminalMatcher(self, grammar_matcher)
