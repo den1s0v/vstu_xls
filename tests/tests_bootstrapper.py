d3cee1a4429bb146202e1e494b678b011d4269cd
Brookit <brookit18@gmail.com>
2025-05-13 19:31:54 +0300

    Fix import antipattern issue

--- tests/tests_bootstrapper.py
+++ tests/tests_bootstrapper.py
@@ -7,4 +7,4 @@ import sys
 def init_testing_environment():
     """ Allow Python to load code from src/ dir seamlessly. """
     src_path = Path(__file__).parent.parent / "src"
-    sys.path.insert(0, str(src_path.resolve()))
+    sys.path.insert(1, str(src_path.resolve()))
