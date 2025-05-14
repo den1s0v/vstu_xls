f51299ae0175728e4529f764cb169de6a5b7cb7c
Brookit <brookit18@gmail.com>
2025-05-14 20:12:36 +0300

    Add xlsx grid tests

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -16,24 +16,24 @@ testdata_path = current_rootpath() / "tests" / "test_data"
 
 class GridTestCase(unittest.TestCase):
     def test_in_1(self):
-        grid1 = testdata_path / "grid1.tsv"
-        grid2 = testdata_path / "grid1.xlsx"
-        gs = [TxtGrid(grid1.read_text()), ExcelGrid(openpyxl.load_workbook(grid2).active)]
-
+        gs = [
+            TxtGrid((testdata_path / "grid1.tsv").read_text()),
+            ExcelGrid(openpyxl.load_workbook(testdata_path / "grid1.xlsx").active),
+        ]
         for g in gs:
-            print(f"Testing {g}")
-            gw = g.getView()
-            print("grid.getBoundingBox:", g.getBoundingBox())
+            dshift = 1 if isinstance(g, ExcelGrid) else 0
+            gw = g.get_view()
+            print("grid.get_bounding_box:", g.get_bounding_box())
             print("grid-view range:", gw)
 
-            self.assertEqual(tuple(gw), (0, 0, 9, 9))
-            self.assertEqual((0, 0, 9, 9), tuple(gw))
+            self.assertEqual(tuple(gw), (dshift, dshift, 9, 9))
+            self.assertEqual((dshift, dshift, 9, 9), tuple(gw))
 
             # Справа НАЛЕВО, снизу ВВЕРХ:
             for cell in gw.iterate_cells((LEFT, UP)):
                 print(cell)
 
-            col = gw.getRegion(Box(0, 0, 1, 9))
+            col = gw.get_region(Box(dshift, dshift, 1, 9))
             print("col-view range:", col)
             content_list = []
             # for point in col.iterate_points( [UP, RIGHT] ):
@@ -50,7 +50,7 @@ class GridTestCase(unittest.TestCase):
             # self.assertEqual(col_chars, '87654321')
             self.assertEqual(col_chars, "12345678")
 
-            row = gw.getRegion(Box(0, 0, 9, 1))
+            row = gw.get_region(Box(dshift, dshift, 9, 1))
             print("row-view range:", row)
             content_list = []
             for cell in row.iterate_cells():
@@ -62,60 +62,106 @@ class GridTestCase(unittest.TestCase):
 
             self.assertEqual(row_chars, "ABCDEFGH")
 
-            rect = gw.getRegion(Box(3, 3, 3, 3))
+            rect = gw.get_region(Box(3 + dshift, 3 + dshift, 3, 3))
             print("rect-view range:", rect)
-            for d in Direction._cache.values():
-                outer = rect.lookOutside(d)
+            for d in Direction.known_instances():
+                outer = rect.look_outside(d)
                 chars = (cell.cell.content for cell in outer.iterate_cells())
                 print("rect-view range, %s:" % d.prop_name, outer)
                 print("                   ", "".join(chars))
 
             print()
-            rect = gw.getRegion(Box(2, 2, 5, 5))
+            rect = gw.get_region(Box(2 + dshift, 2 + dshift, 5, 5))
             print("rect-view range:", rect)
-            for d in Direction._cache.values():
-                outer = rect.lookOutside(d, distance=2)
+            for d in Direction.known_instances():
+                outer = rect.look_outside(d, distance=2)
                 chars = (cell.cell.content for cell in outer.iterate_cells())
                 print("rect-view range, %s:" % d.prop_name, outer)
                 print("                   ", "".join(chars))
 
             print()
             print("# zero distance")
-            rect = gw.getRegion(Box(2, 2, 4, 4))
+            rect = gw.get_region(Box(2 + dshift, 2 + dshift, 4, 4))
             print("rect-view range:", rect)
-            for d in Direction._cache.values():
-                outer = rect.lookOutside(d, distance=0)
+            for d in Direction.known_instances():
+                outer = rect.look_outside(d, distance=0)
                 # chars = (cell.cell.content for cell in outer.iterateCells())
                 print("rect-view range, %s:" % d.prop_name, outer)
                 # print('                   ', ''.join(chars))
 
             print()
             print("# ignored distance")
-            rect = gw.getRegion(Box(2, 2, 4, 4))
+            rect = gw.get_region(Box(2 + dshift, 2 + dshift, 4, 4))
             print("rect-view range:", rect)
-            for d in Direction._cache.values():
-                outer = rect.lookOutside(d, distance=-10)  # ignored distance
+            for d in Direction.known_instances():
+                outer = rect.look_outside(d, distance=-10)  # ignored distance
                 chars = (cell.cell.content for cell in outer.iterate_cells())
                 print("rect-view range, %s:" % d.prop_name, outer)
                 print("                   ", "".join(chars))
 
             print()
             print("# too large distance, clamp.")
-            rect = gw.getRegion(Box(2, 2, 4, 4))
+            rect = gw.get_region(Box(2 + dshift, 2 + dshift, 4, 4))
             print("rect-view range:", rect)
-            for d in Direction._cache.values():
-                outer = rect.lookOutside(d, distance=999)  # too large distance, clamp.
+            for d in Direction.known_instances():
+                outer = rect.look_outside(d, distance=999)  # too large distance, clamp.
                 chars = (cell.cell.content for cell in outer.iterate_cells())
                 print("rect-view range, %s:" % d.prop_name, outer)
                 print("                   ", "".join(chars))
 
     def test_in_2(self):
-        print("test xlsx 2")
         workbook = openpyxl.load_workbook(testdata_path / "grid2.xlsx")
         g = ExcelGrid(workbook.active)
-        gw = g.getView()
-        for cell in gw.iterate_cells():
-            print(cell)
+        gw = g.get_view()
+        self.assertEqual(gw.get_cell_view((1, 1)).cell.content, "a")
+        self.assertEqual(gw.get_cell_view((1, 3)).cell.content, "x")
+        self.assertEqual(gw.get_cell_view((2, 3)).cell.content, "m")
+        self.assertEqual(gw.get_cell_view((3, 1)).cell.content, "b")
+        self.assertEqual(gw.get_cell_view((3, 2)).cell.content, "y")
+
+        self.assertEqual(gw.get_cell_view((4, 1)).cell.content, "merged")
+        self.assertEqual(gw.get_cell_view((4, 1)).size.h, 3)
+        self.assertEqual(gw.get_cell_view((4, 1)).size.w, 2)
+
+    def test_in_3(self):
+        workbook = openpyxl.load_workbook(testdata_path / "grid3.xlsx")
+        g = ExcelGrid(workbook.active)
+        gw = g.get_view()
+        self.assertEqual(gw.get_cell_view((1, 1)).cell.content, "a")
+        self.assertEqual(gw.get_cell_view((2, 1)).cell.content, "b")
+        self.assertEqual(gw.get_cell_view((3, 1)).cell.content, "c")
+        self.assertEqual(gw.get_cell_view((4, 1)).cell.content, "d")
+        self.assertEqual(gw.get_cell_view((5, 1)).cell.content, "e")
+        self.assertEqual(gw.get_cell_view((6, 1)), None)
+
+        self.assertEqual(gw.get_cell_view((1, 2)).cell.content, "x")
+        self.assertEqual(gw.get_cell_view((2, 2)).cell.content, "y")
+        self.assertEqual(gw.get_cell_view((3, 2)).cell.content, "z")
+
+        self.assertEqual(gw.get_cell_view((4, 2)).cell.content, "i")
+        self.assertTrue("bold" in gw.get_cell_view((4, 2)).cell.style.font_style)
+
+        self.assertEqual(gw.get_cell_view((5, 2)).cell.content, "j")
+        self.assertEqual(gw.get_cell_view((5, 2)).size.h, 2)
+        self.assertEqual(gw.get_cell_view((5, 2)).size.w, 1)
+        self.assertEqual(gw.get_cell_view((5, 3)).cell.content, "j")
+        self.assertTrue("italic" in gw.get_cell_view((5, 3)).cell.style.font_style)
+
+        self.assertEqual(gw.get_cell_view((6, 2)).cell.content, "k")
+
+        self.assertEqual(gw.get_cell_view((2, 3)).cell.content, "m")
+        self.assertEqual(gw.get_cell_view((2, 3)).size.h, 2)
+        self.assertEqual(gw.get_cell_view((2, 3)).size.w, 2)
+
+        self.assertEqual(gw.get_cell_view((4, 3)).cell.content, "s")
+        self.assertEqual(gw.get_cell_view((6, 3)).cell.content, "_")
+
+        self.assertEqual(gw.get_cell_view((1, 4)).cell.content, "t")
+        self.assertEqual(gw.get_cell_view((4, 4)).cell.content, "w")
+        self.assertEqual(gw.get_cell_view((5, 4)).cell.content, "p")
+        self.assertTrue("bold" in gw.get_cell_view((5, 4)).cell.style.font_style)
+        self.assertTrue("italic" in gw.get_cell_view((5, 4)).cell.style.font_style)
+        self.assertEqual(gw.get_cell_view((6, 4)), None)
 
 
 if __name__ == "__main__":
