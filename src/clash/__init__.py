4cc329cf452487b0235f42691e0f2ef09fe02b0d
den1s0v <hello-lumen@ya.ru>
2025-05-22 20:47:15 +0300

    Untested clash resolving susbsystem

--- src/clash/__init__.py
+++ src/clash/__init__.py
@@ -3,87 +3,149 @@
 
 На вход подаётся набор элементов, которые могут пересекаться между собой в произвольном соотношении.
 На выходе формируется набор таких подмножеств данных элементов, что в каждом подмножестве:
-    • ни один элемент с другим не пересекается (не конфликтует), и 
-    • больше ни одного элемента в него добавить нельзя.
+    • ни один элемент не пересекается (не конфликтует) с другим, и 
+    • нельзя добавить в него больше ни одного элемента, не нарушив предыдущее условие.
 Набор подмножеств формируется таким образом, что каждый из входных элементов присутствует хотя бы в одном из подмножеств.
 Гарантируется, что все нетривиально различные раскладки/группировки элементов представлены в результате).
 
 Задача решается в абстрактных терминах, 
 так что вызывающий код волен самостоятельно определять, 
 что есть накладка между элементами.
-Чтобы указать, что некоторая пара элементов конфликтует, нужно снабдить  эти элементы одинаковыми компонентами.
+Чтобы указать, что некоторая пара элементов конфликтует, нужно снабдить эти элементы одинаковыми компонентами.
 
 
 """
 
-from typing import Any, Hashable, Iterable
+from functools import cache
+from typing import Any, Hashable, Iterable, override
 # from collections import Or
 
-from dataclasses import Field, dataclass
+from dataclasses import Field, dataclass, asdict
 from adict import adict
 
+from clash.clashing_element import *
+from utils import global_var
 
-_pair_compatibity_checker: callable = None
 
 
 def find_combinations_of_compatible_elements(
     elements: Iterable, 
-    pair_compatibity_checker=None, 
-    components_getter=None) -> list:
+    pair_compatibility_checker=None, 
+    components_getter=None) -> list[set]:
     """ Главная функция .
     """
-    assert pair_compatibity_checker or components_getter, "Any of parameters: `pair_compatibity_checker` or `components_getter` should be set!"
+    
+    assert pair_compatibility_checker or components_getter, "Any of parameters: `pair_compatibility_checker` or `components_getter` should be set!"
+    
+    # # Вспомогательное для объедиенния и наполнения компонентов
+    # hash2component: dict[int, ClashingComponent] = {}
+    
+    # def get_component(component_obj, container: ClashingContainer) -> ClashingComponent:
+    #     """ Get or create component """
+    #     h = hash(component_obj)
+    #     comp = hash2component.get(h)
+    #     if not comp:
+    #         hash2component[h] = comp = ClashingComponent(component_obj)
+    #     comp.belonds_to.add(container)
+    #     return comp
 
-    return []
+    # # Подготовить объекты, упаковав их в наши обёртки
+    # clashing_elements = []
+    
+    # for element in elements:
+    #     if components_getter:
+    #         el = ClashingContainer(
+    #             obj=element, 
+    #             components={
+    #                 get_component(component, element)
+    #                 for component in components_getter(element)
+    #             }
+    #         )
+    #     else:
+    #         el = ClashingElement(obj=element)
+
+    #     clashing_elements.append(el)
+    
+    clashing_set = ClashingElementSet.make(
+            elements, 
+            # pair_compatibility_checker, 
+            components_getter,
+    )
+
+    set_pair_compatibility_checker(pair_compatibility_checker)
+
+    # return ClashResolver({
+    #     hash(ce): ce
+    #     for ce in clashing_set
+    # }).resolve()
+    clash_sets = resolve_clashes(clashing_set)
+    # Extract objects back
+    return [
+        # {clash_elem.obj for clash_elem in clash_set}
+        clash_set.bare
+        for clash_set in clash_sets
+    ]
     
 
 
-class ClashResolver:
-    def resolve(self):
-        ...
+# @dataclass()
+# class ClashResolver:
+#     hash2element: dict[int, 'ClashingElement']
+    
+#     def resolve(self):
+#         ...
+
+        
+# def resolve_clashes(clashing_elements: list['ClashingElement']) -> list[set['ClashingElement']]:
+def resolve_clashes(clashing_set: 'ClashingElementSet') -> list['ClashingElementSet']:
+    """ Нахождение всех локально оптимальных раскладок элементов, где они не пересекаются """
+    # кластеризация накладок.
+    
+    # Выделение неконфликтующих раскладок.
+    arrangements: list['ClashingElementSet'] = []
+
+    # Элементы, полностью освобождённые при формировании раскладок (для них уже рскладки формировать не нужно, иначе это будет просто дублирование)
+    released_elements = set()  # was ever in an arrangement, to avoid trivial duplicates.
+
+    # Внутри кластера перебираем все элементы по очереди:
+    for elem in clashing_set:
+        if elem in released_elements:
+            continue
+        
+        released_elements.add(elem)  # Этот мы точно освободим
+        
+        # Сделать текущий свободным (убрать все мешающие).
+        directly_clashing = elem.all_clashing_among(clashing_set)
+        
+        if not directly_clashing:
+            # Ничего ни с чем и не конфликтовало
+            arrangement = clashing_set.clone()
+            arrangements.append(arrangement)
+            break
+        
+        partially_free_set = clashing_set.with_removed(*directly_clashing)
+
+        # Все освобождённые идут в раскладку сразу и исключаются из дальнейшего перебора по внешнему циклу.
+        released = partially_free_set.free_subset()
+        released_elements |= released
+        
+        unresolved = partially_free_set.with_removed(released)
+        
+        # Все несвободные группируются в новый кластер и подаются в рекурсивный вызов.
+        sub_arrangements = resolve_clashes(unresolved)  # recursive call !
+        
+        # Полученные под-раскладки комбинируются с текущими свободными.
+        for sa in sub_arrangements:
+            arrangements.append(released.clone | sa)
+
+    # Выделение неконфликтующих раскладок.
+    # Внутри кластера перебираем все элементы по очереди:
+    # Сделать текущий свободным (убрать все мешающие).
+    # Все освобождённые идут в раскладку сразу и исключаются из дальнейшего перебора по внешнему циклу.
+    # Все несвободные группируются в новый кластер и подаются в рекурсивный вызов.
+    # Полученные под-раскладки комбинируются с текущими свободными.
+
+    return arrangements
         
 
 
-
-@dataclass()
-class ObjWithDataWrapper(Hashable):
-    """ A generic wrapper for an arbitrary object, 
-        optionally associated with some arbitrary data
-    """
-    obj: Hashable
-    data: adict = Field(default_factory=adict)
-
-    def __hash__(self):
-        return self.obj.__hash__()
-
-
-
-class ClashingElement(ObjWithDataWrapper):
-    # clashes_with: set['ClashingElement']
-    # cluster: None | int
-    
-    def clashes_with(self, other: 'ClashingElement'):
-        assert _pair_compatibity_checker
-        return not _pair_compatibity_checker(self, other)
-    
-    def kee(self):
-        return not self.clashes_with
-
-    
-
-class ClashingContainer(ClashingElement):
-    components: set['ClashingComponent']
-    
-    def clashes_with(self, other: 'ClashingContainer'):
-        assert isinstance(other, ClashingContainer), type(other)
-        return bool(self.components & other.components)
-    
-    def is_free(self):
-        return all(
-            self in i.belonds_to and len(i.belonds_to) == 1
-            for i in self.components)
-
-
-class ClashingComponent(ObjWithDataWrapper):
-    belonds_to: set['ClashingContainer']
-    
---
+++ src/clash/clashing_element.py
@@ -0,0 +1,188 @@
+# clashing_element.py
+
+from functools import cache
+from typing import Any, Hashable, Iterable, override
+# from collections import Or
+
+from dataclasses import Field, dataclass, asdict
+from adict import adict
+
+
+# Функция (obj1, obj2) -> bool
+_pair_compatibility_checker: callable = None
+
+
+def set_pair_compatibility_checker(func):
+    global _pair_compatibility_checker
+    assert callable(func)
+    _pair_compatibility_checker = func
+
+
+@dataclass()
+class ObjWithDataWrapper(Hashable):
+    """ A generic wrapper for an arbitrary object, 
+        optionally associated with some arbitrary data
+    """
+    obj: Hashable
+    data: adict = Field(default_factory=adict)
+
+    def __hash__(self):
+        try:
+            return hash(self.obj)
+        except:
+            # Fallback: instance identity
+            return id(self.obj)
+
+
+
+class ClashingElement(ObjWithDataWrapper):
+    # clashes_with: set['ClashingElement']
+    # cluster: None | int
+    
+    def clashes_with(self, other: 'ClashingElement') -> bool:
+        assert _pair_compatibility_checker
+        return not _pair_compatibility_checker(self.obj, other.obj)
+    
+    def all_clashing_among(self, others) -> set['ClashingElement']:
+        """ Note: Does not clash to itself """
+        return {other for other in others if (other is not self) and self.clashes_with(other)}
+    
+    def all_independent_among(self, others) -> set['ClashingElement']:
+        """ Note: Not independent from itself """
+        return {other for other in others if (other is not self) and not self.clashes_with(other)}
+    
+    def clone(self):
+        return type(self)(**asdict(self))
+    
+    def on_remove_from_set(self):
+        # nothing to do
+        pass
+    
+
+@dataclass()
+class ClashingContainer(ClashingElement):
+    components: set['ClashingComponent']
+    # _all_directly_overlapping_cache = None
+    
+    def clashes_with(self, other: 'ClashingContainer') -> bool:
+        assert isinstance(other, ClashingContainer), type(other)
+        return bool(self.components & other.components)
+    
+    def is_free(self):
+        return all(
+            self in i.belonds_to and len(i.belonds_to) == 1
+            for i in self.components)
+
+    @cache
+    def all_directly_overlapping(self, ) -> set['ClashingContainer']:
+        return {other 
+                for component in self.components
+                for other in component.belonds_to
+                if other is not self
+               }
+    
+    def all_clashing_among(self, others) -> set['ClashingContainer']:
+        # optimized version
+        return set(others) & self.all_directly_overlapping()
+    
+    def all_independent_among(self, others) -> set['ClashingContainer']:
+        # optimized version
+        return set(others) - self.all_directly_overlapping()
+    
+    def clone(self):
+        fields = asdict(self)
+        # clone components as well
+        fields['components'] = {cm.clone() for cm in self.components}
+        return type(self)(**fields)
+    
+    def on_remove_from_set(self):
+        # delete from related components
+        for component in self.components:
+            component.belonds_to.remove(self)
+
+
+@dataclass()
+class ClashingComponent(ObjWithDataWrapper):
+    belonds_to: set['ClashingContainer']
+    
+    def clone(self):
+        fields = asdict(self)
+        # re-create set
+        fields['belonds_to'] = set(self.belonds_to)
+        return type(self)(**fields)
+    
+    
+    
+
+# @dataclass()
+class ClashingElementSet(set['ClashingElement']):
+    # elements: set['ClashingElement']
+    
+    @override
+    def remove(self, *elements: 'ClashingElement'):
+        for element in elements:
+            # trigger changes
+            element.on_remove_from_set()
+            # remove as usual
+            super().remove(element)
+
+    def with_removed(self, *elements: 'ClashingElement'):
+        s = self.clone()
+        s.remove(*elements)
+        return s
+
+    def free_subset(self):
+        """ Make a subset that it contains only elements not clashing with any other (in this) """
+        s = type(self)()
+        for el in self:
+            if not el.all_clashing_among(*self):
+                s.add(el.clone())
+                
+        return s
+
+    @classmethod
+    def make(cls, 
+            elements: Iterable, 
+            # pair_compatibity_checker=None, 
+            components_getter=None) -> 'ClashingElementSet':
+
+        # Вспомогательное для объедиенния и наполнения компонентов
+        hash2component: dict[int, ClashingComponent] = {}
+        
+        def get_component(component_obj, container: ClashingContainer) -> ClashingComponent:
+            """ Get or create component """
+            h = hash(component_obj)
+            comp = hash2component.get(h)
+            if not comp:
+                hash2component[h] = comp = ClashingComponent(component_obj)
+            comp.belonds_to.add(container)
+            return comp
+
+        # Подготовить объекты, упаковав их в наши обёртки
+        clashing_elements = []
+        
+        for element in elements:
+            if components_getter:
+                el = ClashingContainer(
+                    obj=element, 
+                    components={
+                        get_component(component, element)
+                        for component in components_getter(element)
+                    }
+                )
+            else:
+                el = ClashingElement(obj=element)
+
+            clashing_elements.append(el)
+        
+        return cls(clashing_elements)
+    
+    def clone(self) -> 'ClashingElementSet':
+        """Deep clone"""
+        return type(self)(el.clone() for el in self)
+    
+    def get_bare_objs(self) -> set:
+        # Extract objects back
+        return {clash_elem.obj for clash_elem in self}
+
+
