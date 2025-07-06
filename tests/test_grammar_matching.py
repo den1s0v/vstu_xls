04c1e26828afb83716ac760baffe09b4fcf30b3b
den1s0v <hello-lumen@ya.ru>
2025-07-07 00:19:45 +0300

    tests: using new functions to verify both grid type

--- tests/test_grammar_matching.py
+++ tests/test_grammar_matching.py
@@ -6,14 +6,26 @@ init_testing_environment()
 
 from pathlib import Path
 
+from converters.xlsx import ExcelGrid
 from converters.text import TxtGrid
 from grammar2d import read_grammar, GrammarMatcher
 
 
 class GrammarMatchingTestCase(unittest.TestCase):
-    def test_txt(self):
-        g = TxtGrid(Path('test_data/grid1.tsv').read_text())
+    @classmethod
+    def setUpClass(cls):
+        cls.grid1_t = TxtGrid(Path('test_data/grid1.tsv').read_text())
+        cls.grid1_x = ExcelGrid.read_xlsx(Path('test_data/grid1.xlsx'))
+
+        cls.simple_grammar = read_grammar('test_data/simple_grammar_txt.yml')
+
+    def _test_txt_debug(self):
+        # g = TxtGrid(Path('test_data/grid1.tsv').read_text())
+        g = ExcelGrid.read_xlsx(Path('test_data/grid1.xlsx'))
         # gw = g.get_view()
+        print()
+        print("Grid had beed red.")
+        print()
 
         grammar = read_grammar('test_data/simple_grammar_txt.yml')
 
@@ -49,7 +61,41 @@ class GrammarMatchingTestCase(unittest.TestCase):
         print()
         # pprint(grammar.patterns)
 
+    def test_grid1(self):
+        gm = GrammarMatcher(grammar=self.simple_grammar)
+
+        for g in (
+                self.grid1_x,
+                self.grid1_t,
+                self.grid1_x,
+        ):
+            print('using grid:', g)
+            matched_documents = gm.run_match(g)
+
+            self.assertEqual(1, len(matched_documents))
+            root = matched_documents[0]
+            self.assertEqual((9, 9), root.box.size)
+
+            self.assertEqual((8, 1), root.component2match['letters'].box.size)
+            self.assertEqual((1, 8), root.component2match['numbers'].box.size)
+            self.assertEqual((8, 8), root.component2match['field'].box.size)
+
+            self.assertEqual(('ABCDEFGH'), ''.join(root.component2match['letters'].get_text()))
+            self.assertEqual(('87654321'), ''.join(root.component2match['numbers'].get_content()))
+
+            root_content = root.get_content()
+            root_content['field'] = set(root_content['field'])
+            self.assertEqual({
+                'field': {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', '*', '*', '*', '*', '*', '*', '*',
+                          '*', '*',
+                          '*', '*', '*', },
+                'numbers': ['8', '7', '6', '5', '4', '3', '2', '1'],
+                'letters': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']},
+                root_content)
+
 
 if __name__ == '__main__':
     # unittest.main()
-    GrammarMatchingTestCase.test_txt(...)
+    # GrammarMatchingTestCase._test_txt_debug(...)
+    GrammarMatchingTestCase.setUpClass()
+    GrammarMatchingTestCase.test_grid1(GrammarMatchingTestCase())
