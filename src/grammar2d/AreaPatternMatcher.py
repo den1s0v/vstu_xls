ef68da6c5c3a26720e3e373df5f755290f89bafd
den1s0v <hello-lumen@ya.ru>
2025-07-19 19:34:55 +0300

    feat: Implement v3 of area-pattern matching (same fast but more "capable")

--- src/grammar2d/AreaPatternMatcher.py
+++ src/grammar2d/AreaPatternMatcher.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Self
 
 from loguru import logger
 
@@ -15,6 +16,30 @@ import grammar2d.GrammarMatcher as ns
 from grid import Region
 
 from clash import find_combinations_of_compatible_elements
+
+@dataclass(slots=True)
+class MatchingPlan:
+    component_matches_list: list[tuple[PatternComponent, list[Match2d]]]
+
+    def get_position(self, component_i: int = 0):
+        return PositionInMatchingPlan(self, component_i)
+
+    def __getitem__(self, item):
+        return self.component_matches_list[item]
+
+    def __len__(self):
+        return len(self.component_matches_list)
+
+
+@dataclass(slots=True)
+class PositionInMatchingPlan:
+    plan: MatchingPlan
+    component_i: int = 0
+
+    # match_i: int = 0
+
+    def clone(self) -> Self:
+        return type(self)(self.plan, self.component_i)
 
 
 @dataclass
@@ -371,6 +396,242 @@ class AreaPatternMatcher(PatternMatcher):
                 current_wave.append(m2)
         return current_wave
 
+    def find_match_candidates_3(self, region: Region = None) -> list[Match2d]:
+        """ Find all matches no matter if they do apply simultaneously or not.
+         The patterns may be not applicable if they do overlap.
+
+         Каждое найденное совпадение существует как бы независимо от остальных.
+
+         Вариант с оптимизацией обхода кандидатов по близости к текущей области совпадения.
+         Отсекаются далёкие варианты.
+         Идея была брать x2 от минимального найденного расстояния,
+          но за приемлемое время работает только с x1 (т.е. только лучшее расстояние + 1 единица длины на запас)
+        """
+
+        pattern = self.pattern
+        gm = self.grammar_matcher
+
+        # 1. Найти все матчи-кандидаты для всех паттернов-компонентов.
+        # Включая потенциально пустые наборы матчей для опциональных компонентов.
+
+        component_matches_list: list[tuple[PatternComponent, list[Match2d]]] = []
+
+        for pattern_component in pattern.components:
+            occurrences = gm.get_pattern_matches(pattern_component.subpattern, region)
+
+            if not occurrences and not pattern_component.optional:
+                logger.info(f'''NO MATCH: pattern `{self.pattern.name
+                }` cannot have any matches since its required component `{pattern_component.name}` has no matches.''')
+                return []
+
+            # Убрать совпадения паттерна, слишком неточные для этого компонента
+            occurrences = list(filter(
+                lambda m: m.precision >= pattern_component.precision_threshold,
+                occurrences))
+
+            component_matches_list.append((pattern_component, occurrences))
+
+        # 2. Получить все "развёрнутые" области потенциального местонахождения родителя-area
+        #    для последующего комбинирования.
+        #   Записать в метаданные: match.data.parent_location[pattern]: RangedBox
+
+        for pattern_component, match_list in component_matches_list:
+            for component_match in match_list:
+                # Получить область потенциального местонахождения родителя-area
+                parent_location = pattern_component.get_ranged_box_for_parent_location(component_match)
+                if component_match.data.parent_location is None:
+                    # Если словарь не создан
+                    component_match.data.parent_location = dict()
+                # Записать в метаданные
+                component_match.data.parent_location[(pattern.name, pattern_component.name)] = parent_location
+
+        # 3. Найти комбинации из паттернов всех обязательных компонентов, дающие непустой матч для area.
+        # 3.1. После обязательных, добавлять в каждый матч опциональные компоненты, попавшие в "зону влияния"
+        # найденной области (он не может быть опущен, если лежит внутри области,
+        # обозначенной обязательными компонентами).
+
+        # TODO: пересмотреть описание ↓
+        # Каждое действие добавления нового компонента в матч носит вероятностный и ветвящийся характер.
+        #  Поэтому заводим стек для уровней/ветвей наращивания вглубь дерева принятия решений о добавлении компонентов.
+        #  На листьях дерева (конец рекурсии) будут готовые, полностью заполненные матчи нашего паттерна.
+        #  (?? На каждом уровне будем ограничивать ветвление лучшими вариантами, которые будут отбираться по принципу:
+        #  дельта показателя к лучшему варианту добавить компонент умножается на 2 (коэффициент),
+        #  это определяет порог отсечения.
+        #  В случае нулевой дельты взять порог равным 1 лишней клетке. ??)
+        #  По отобранным кандидатам углубляемся внутрь (рекурсия).
+
+        # Сортировать: все опциональные в конце, сначала внутренние, по возрастанию числа матчей
+        component_matches_list.sort(key=lambda t: (
+            not t[0].optional,
+            not t[0].inner,
+            len(t[1]),  # ↑
+        ))
+
+        plan = MatchingPlan(component_matches_list)
+
+        complete_matches: list[Match2d] = []
+
+        # Будем считать, что компонентов ненулевое кол-во,
+        # и (?) первый (после сортировки) будет обязательным (?)
+        # Запросим варианты главного совпадения по всем элементам первого компонента;
+        # по остальным уровням рекурсии задано максимум 1 результат.
+        assert component_matches_list
+        # assert not plan[0][0].optional, plan[0]
+        first_comp_matches_count = len(plan[0][1])
+
+        complete_matches = self._best_matches(
+            plan.get_position(),
+            None,
+            first_comp_matches_count
+        )
+
+        # Получить окончательную область совпадения area для всех кандидатов (с учётом потенциальных выносов)
+        for m in complete_matches:
+            combined_ranged_box: RangedBox = m.data.ranged_box
+            m.box = combined_ranged_box.minimal_box().to_box()
+            # recalc precision
+            m.calc_precision(force=True)
+
+        return complete_matches
+
+    def _best_matches(self, plan_pos: PositionInMatchingPlan, existing_match: Match2d | None = None, max_results=1) -> (
+            list)[
+        Match2d]:
+        """
+        Рекурсивный поиск совпадений (матчей) базового паттерна.
+        Эта функция подбирает следующий (переданный) компонент паттерна, на котором может построить матч.
+        Сначала ранжируем все потенциально подходящие матчи компонента по принципу близости к текущей области совпадения
+         (для равных — по точности и координате),
+         и, начиная с ближайших, пробуем рекурсивно найти матч паттерна.
+        Если для очередного варианта текущего компонента матч найден, то дальше не ищем и возвращаем его.
+
+        Особенностью этой реализации является то, что existing_match может отсутствовать,
+        как на начальном, так и на последующих этапах матчинга
+        из-за опциональных компонентов (если они все такие).
+
+        Returns list of 0 to `max_results` elements.
+        """
+        plan = plan_pos.plan
+        component, match_list = plan[plan_pos.component_i]
+        ### logger.debug(f'_best_matches: entered with component `{component.name}` at pos {plan_pos.component_i}, ' f'requested {max_results} max_results.')
+        ###
+
+        # 1. ранжируем всех кандидатов по расположению относительно текущего матча
+        # 1.1. Получить расстояние от текущей позиции
+        # и записать её в каждый match компонента под ключом в виде box текущей позиции.
+        # 1.2. Проранжироать и найти лучшую дельту.
+
+        rb1 = existing_match.data.ranged_box if existing_match else None
+        size_constraint = self._get_pattern_size_constraint()
+
+        distance_rb_match_list: list[tuple[float, RangedBox, Match2d]] = []
+
+        for component_match in match_list:
+
+            # Скомбинировать области для проверки
+            rb2 = component_match.data.parent_location[(self.pattern.name, component.name)]
+            if rb1:
+                combined_rb = rb1.combine(rb2)
+            else:
+                combined_rb = rb2
+
+            # наложить ограничения на размеры области
+            if combined_rb and size_constraint:
+                combined_rb = combined_rb.restricted_by_size(*size_constraint)
+
+            if not combined_rb: continue
+
+            if existing_match and not self._check_component_relations(existing_match, (component, component_match)):
+                # не подошёл, дальше не рассматриваем
+                continue
+
+            # Расстояние до подходящего кандидата
+            if existing_match:
+                distance = component.calc_distance_of_match_to_box(component_match, existing_match.box)
+            else:
+                distance = 0  # constant
+
+            distance_rb_match_list.append((distance, combined_rb, component_match))
+
+        # Sort to have the best alternatives first.
+        distance_rb_match_list.sort(key=lambda t: (
+            t[0],  # distance, ↑
+            -t[2].precision,  # match precision, ↓
+            sum(t[2].box.position),  # coordinates sum, ↑
+        ))
+
+        # 2. вычисляем порог отсечки и фильтруем кандидатов по этой отсечке для расстояния
+        min_distance = min((t[0] for t in distance_rb_match_list), default=0)
+
+        # CUTOFF_COEF = 2.0
+        CUTOFF_COEF = 1  # !!!!!!! Минимум ←
+        CUTOFF_MIN_DIST = 1
+
+        # Отсечка вдвое больше минимальной, но не меньше 1 (в случае, если минимальная равна нулю)
+        cutoff_distance = max(min_distance * CUTOFF_COEF, CUTOFF_MIN_DIST)
+
+        # Перебираем варианты и спускаем их на уровень ниже по рекурсии.
+
+        next_level = plan_pos.component_i + 1
+        next_pos = plan.get_position(next_level)
+        can_recurse = next_level < len(plan)
+
+        complete_matches: list[Match2d] = []
+
+        for distance, combined_rb, component_match in distance_rb_match_list:
+
+            if distance > cutoff_distance:
+                # отсекаем далёкие варианты
+                continue
+
+            # попытаться найти полный матч из текущего частичного матча
+            if existing_match:
+                m2 = existing_match.clone()
+            else:
+                m2 = Match2d(self.pattern,
+                             box=None,
+                             precision=0,
+                             component2match={}
+                             )
+            m2.component2match[component.name] = component_match
+            m2.recalc_box()
+            # m2.precision += component_match.precision * component.weight
+            m2.data.ranged_box = combined_rb
+
+            if can_recurse:
+                sub_results = self._best_matches(next_pos, m2, 1)  # TODO: 1 ??  max_max_results ??
+                complete_matches.extend(sub_results)
+            else:
+                # Последний компонент добавлен
+                complete_matches.append(m2)
+
+            if len(complete_matches) >= max_results:
+                break
+
+        if not complete_matches and component.optional:
+            # компонент опциональный, здесь его не будет
+            # (он не попал в нашу область),
+            # но матч остаётся.
+
+            if not existing_match:
+                existing_match = Match2d(self.pattern,
+                                         box=None,
+                                         precision=0,
+                                         component2match={}
+                                         )
+            if can_recurse:
+                m2 = existing_match
+                sub_results = self._best_matches(next_pos, m2, 1)  # TODO: 1 ??  max_max_results ??
+                complete_matches.extend(sub_results)
+            else:
+                # Последний компонент добавлен
+                complete_matches.append(existing_match)
+
+        ### logger.debug(f'_best_matches: ← leaving with {len(complete_matches)} results.')
+        ###
+        # limit & return
+        return complete_matches[:max_results]
+
     def filter_candidates(self, match_candidates: list[Match2d]) -> list[Match2d]:
         """ Filter given matches so all returned matches do not overlap and the combination seems to be the best.
 
@@ -379,7 +640,7 @@ class AreaPatternMatcher(PatternMatcher):
         """
 
         ###
-        # logger.info(f'match_candidates: {len(match_candidates)}')
+        logger.info(f'filtering candidates: {len(match_candidates)}')
         # logger.info(f'match_candidates: {(match_candidates)}')
 
         arrangements = find_combinations_of_compatible_elements(
