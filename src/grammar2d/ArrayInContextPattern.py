4fd33dcfa4949d4bd31a90d3f8ad335c91acd47a
den1s0v <hello-lumen@ya.ru>
2025-07-23 12:43:57 +0300

    feat: Add context-dependent kind of Array pattern

---
+++ src/grammar2d/ArrayInContextPattern.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+from typing import override
+
+from loguru import logger
+
+from grammar2d.ArrayPattern import ArrayPattern
+from utils import sorted_list
+from geom2d import open_range, Point
+import grammar2d.Match2d as m2
+from grammar2d.NonTerminal import NonTerminal
+from grammar2d.Pattern2d import Pattern2d, PatternRegistry
+
+
+@PatternRegistry.register
+@dataclass(kw_only=True, repr=True)
+class ArrayInContext(ArrayPattern):
+    """Массив или ряд однотипных элементов, выровненных друг относительно друга
+        в контексте Area: придерживается границ Area, игнорируя подходящие элементы за его пределами.
+    """
+
+    @classmethod
+    @override
+    def get_kind(cls):
+        return "array-in-context"  # ???
+
+    @classmethod
+    def independently_matchable(cls):
+        """ Returns False as this specific patterns that cannot be matched independently,
+        rather in a context of Area pattern. """
+        return False
+
+    def get_matcher(self, grammar_matcher):
+        from grammar2d.ArrayInContextPatternMatcher import ArrayInContextPatternMatcher
+        return ArrayInContextPatternMatcher(self, grammar_matcher)
---
+++ src/grammar2d/ArrayInContextPatternMatcher.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+from dataclasses import dataclass
+
+from loguru import logger
+
+from geom2d import Box, Direction, RIGHT, DOWN
+from grammar2d import ArrayPattern
+from grammar2d.ArrayPatternMatcher import ArrayPatternMatcher
+from grammar2d.Match2d import Match2d
+from grid import Region
+
+
+@dataclass
+class ArrayInContextPatternMatcher(ArrayPatternMatcher):
+
+    def find_all(self, _region: Region = None) -> list[Match2d]:
+        """ Find all matches within whole document. """
+        # short aliases
+        item = self.pattern.subpattern
+        gm = self.grammar_matcher
+
+        item_occurrences = gm.get_pattern_matches(item)
+
+        if not item_occurrences:
+            return []
+
+        matches = []
+
+        if len(item_occurrences) == 1:
+            # There is only one occurrence, no need to analyze its placement.
+            item_match = item_occurrences[0]
+
+            satisfies = self.pattern.check_constraints_for_bbox(item_match.box)
+
+            if satisfies:
+                # make a Match duplicating the only occurrence.
+                m = Match2d(self.pattern,
+                            precision=item_match.precision,
+                            box=item_match.box,
+                            component2match={0: item_match})
+                matches.append(m)
+            return matches
+
+        # найти ряды элементов, одинаково выровненных вдоль заданного направления
+        matches = self._find_lines(item_occurrences)
+
+        return matches
+
+    def match_exact_region(self, region: Region) -> list[Match2d]:
+        """ Find all matches within given region. """
+        return self.find_all(region)
